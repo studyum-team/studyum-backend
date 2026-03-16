95d91bf038d2423ab76dee9c4a722c5bef5d3a6d
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2026-03-16 15:17:10 +0900

    git init

---
+++ src/main/java/com/studyumbackend/StudyumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.studyumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyumBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyumBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studyumbackend/StudyumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyumBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
