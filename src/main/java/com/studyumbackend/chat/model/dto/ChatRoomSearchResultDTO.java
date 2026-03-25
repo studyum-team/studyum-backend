382908f4591dacba9d41cefbdd6da8c86aa34a8f
Emma10003 <5emma10003@gmail.com>
2026-03-25 10:53:16 +0900

    feature/chatroom-search-re : 스터디룸 검색 mapper

---
+++ src/main/java/com/studyumbackend/chat/model/dto/ChatRoomSearchResultDTO.java
@@ -0,0 +1,34 @@
+package com.studyumbackend.chat.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoomSearchResultDTO {
+    // 채팅방 고유 식별자 (Auto)
+    private long chatRoomId;
+    // 채팅방 카테고리 ID
+    private long categoryId;
+    // 채팅방 이름
+    private String chatRoomName;
+    // 채팅방 소개
+    private String chatRoomDescription;
+    // 채팅방 프로필
+    private String chatRoomImage;
+    // 채팅방 공개 여부
+    private String chatRoomVisibility;
+    // 채팅방 최대 입장 인원
+    private int chatRoomMaxMember;
+    // 채팅방 참여 인원
+    private int chatRoomCurrentMember;
+    // 채팅방 모집 상태
+    private String chatRoomRecruitStatus;
+    // 채팅방 생성 일자
+    private String chatRoomCreatedAt;
+
+    // 카테고리 명칭
+    private String categoryName;
+}
